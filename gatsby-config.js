34c4a452ae007af7a4bc68a2362316010ee313ae
Suresh Pattu <sureshpattu2002@gmail.com>
2021-04-29 15:14:50 +0530

    file path fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,15 @@ module.exports = {
     siteMetadata: {
         title: "Unavu Kaadu (Food Forest)",
         titleTemplate: "",
-        description: "Unavu Kaadu (Food Forest) - Food Forest gardening is a low-maintenance, sustainable, plant-based food production and agroforestry system based on woodland ecosystems, incorporating fruit and nut trees, shrubs, herbs, vines and perennial vegetables which have yields directly useful to humans.",
+        description: "Unavu Kaadu (Food Forest) - Unavu Kaadu gardening is a low-maintenance, sustainable, plant-based food production and agroforestry system based on woodland ecosystems, incorporating fruit and nut trees, shrubs, herbs, vines and perennial vegetables which have yields directly useful to humans.",
         siteUrl: "http://www.unavukaadu.com",
-        image: "/images/logo.png",
+        image: `${__dirname}/src/images/logo.png`,
         twitterUsername: "@unavukaadu",
     },
     plugins: [
         "gatsby-plugin-sass",
+        "gatsby-plugin-sharp",
+        "gatsby-plugin-react-helmet",
         "gatsby-plugin-image",
         {
             resolve: "gatsby-plugin-google-analytics",
@@ -17,21 +19,19 @@ module.exports = {
                 head: true,
             },
         },
-        "gatsby-plugin-react-helmet",
         "gatsby-plugin-sitemap",
         {
             resolve: "gatsby-plugin-manifest",
             options: {
-                icon: "src/images/logo.png",
+                icon: `${__dirname}/src/images/logo.png`,
             },
         },
-        "gatsby-plugin-sharp",
         "gatsby-transformer-sharp",
         {
             resolve: "gatsby-source-filesystem",
             options: {
                 name: "images",
-                path: "./src/images/",
+                path: `${__dirname}/src/images/`,
             },
             __key: "images",
         },
