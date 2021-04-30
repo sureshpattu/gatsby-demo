51619405339122e45491f699eb49db1e29aef01f
Suresh Pattu <sureshpattu2002@gmail.com>
2021-04-30 10:37:42 +0530

    SEO support added

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
         "gatsby-plugin-sharp",
         "gatsby-plugin-react-helmet",
         "gatsby-plugin-image",
+        "gatsby-plugin-offline",
         {
             resolve: "gatsby-plugin-google-analytics",
             options: {
